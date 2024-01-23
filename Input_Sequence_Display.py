1cee1eec43c8fc38d18cac727a55c5a1c96c745b
Anson_Skywalker <36159475+AnsonSkywalker@users.noreply.github.com>
2024-01-23 14:14:58 +0800

    第一个在OBS中完全可用的版本

--- Input_Sequence_Display.py
+++ Input_Sequence_Display.py
@@ -1,14 +1,62 @@
 import time
 from pynput.keyboard import Key, Listener
+import obspython as obs
+import sys
+from io import StringIO
 
+buffer = StringIO()
 keys_currently_pressed = []
 count = 0
 flag = True
 time_last = time.time_ns()
 time_now = 0
+source_name = ""
 
 tickrate = 60 # 每一秒钟记为多少帧，单位Hz
 wait = 5 # 超过多长时间没有输入就忽略本次计数，单位秒
+
+sys.stdout = buffer
+
+def refresh_pressed(props, prop):
+    print("Refresh Pressed")
+    update_text()
+
+def update_text():
+    global source_name
+
+    source = obs.obs_get_source_by_name(source_name)
+    text = buffer.getvalue()
+    if source is not None:
+        settings = obs.obs_data_create()
+        obs.obs_data_set_string(settings, "text", text)
+        obs.obs_source_update(source, settings)
+        obs.obs_data_release(settings)
+
+        obs.obs_source_release(source)
+
+def script_properties():
+	props = obs.obs_properties_create()
+      
+	p = obs.obs_properties_add_list(props, "source", "Text Source", obs.OBS_COMBO_TYPE_EDITABLE,obs.OBS_COMBO_FORMAT_STRING)
+	sources = obs.obs_enum_sources()
+	if sources is not None:
+		for source in sources:
+			source_id = obs.obs_source_get_unversioned_id(source)
+			if source_id == "text_gdiplus" or source_id == "text_ft2_source":
+				name = obs.obs_source_get_name(source)
+				obs.obs_property_list_add_string(p, name, name)
+
+		obs.source_list_release(sources)
+
+	obs.obs_properties_add_button(props, "button", "Refresh", refresh_pressed)
+	return props
+
+def script_update(settings):
+    global source_name
+    source_name = obs.obs_data_get_string(settings, "source")
+    obs.timer_remove(update_text)
+    if source_name != "":
+        obs.timer_add(update_text, 1)
 
 def on_press(key):
     global count,time_last,time_now
@@ -78,5 +126,3 @@ listener = Listener(
 listener.start()
 
 print("监听键位输入序列已启动。当前监听帧率：",tickrate,"Hz")
-while True:
-    pass
