78f9c08f40bd2fe54c39ce7ce304bfdad791c0ea
Anson_Skywalker <36159475+AnsonSkywalker@users.noreply.github.com>
2024-01-23 10:34:37 +0800

    Update Input_Sequence_Display.py

--- Input_Sequence_Display.py
+++ Input_Sequence_Display.py
@@ -4,9 +4,13 @@ from pynput.keyboard import Key, Listener
 keys_currently_pressed = []
 count = 0
 flag = True
+time_last = time.time_ns()
+time_now = 0
+
+tickrate = 60 # 每一秒钟记为多少帧
 
 def on_press(key):
-    global count
+    global count,time_last,time_now
     if key not in keys_currently_pressed:
         keys_currently_pressed.append(key)
         flag = True
@@ -54,8 +58,10 @@ def on_press(key):
         #    key = key.replace("'", "")
         #    key = key.upper()    
         if flag:
-            count = 0
-            print("帧后击键：{}".format(key))      
+            time_now=time.time_ns()
+            count = int((time_now - time_last)/1000000000*tickrate) % 1000 # 帧计数器显然没有那么长的显示需求，所以对一千取模
+            print(count,"帧 ",format(key),flush = True)
+            time_last = time_now    
 
 def on_release(key):
     global n
@@ -68,8 +74,6 @@ listener = Listener(
     on_release=on_release)
 listener.start()
 
+print("监听键位输入序列已启动。当前监听帧率：",tickrate,"Hz")
 while True:
-    print(count,end='')
-    count += 1
-    time.sleep(0.0167)
-    print("\r",end="")
+    pass
